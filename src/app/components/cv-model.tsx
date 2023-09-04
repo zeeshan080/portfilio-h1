66a548d69f9650d12e09ca89d2235b2360c0612e
Zeeshan Aziz <zeeshan922837@gmail.com>
2023-09-04 22:05:49 +0500

    bug fix again

--- src/app/components/cv-model.tsx
+++ src/app/components/cv-model.tsx
@@ -31,7 +31,7 @@ export default function CvModel({}: Props) {
           <DialogHeader>
             <DialogTitle>Edit profile</DialogTitle>
             <DialogDescription>
-              Make changes to your profile here. Click save when you're done.
+              Make changes to your profile here. Click save when you&apos;re done.
             </DialogDescription>
           </DialogHeader>
           <div className="">
